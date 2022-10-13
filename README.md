dd8edcace7a4d63023a2efe39f3b0e5bc848f3db
Jadon <jiadong001@e.ntu.edu.sg>
2022-10-13 16:40:09 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Equivariant-Transporter-Net
+[Project Website](https://haojhuang.github.io/etp_page/)&nbsp;&nbsp;•&nbsp;&nbsp;[PDF](https://arxiv.org/abs/2202.09400)&nbsp;&nbsp;•&nbsp;&nbsp; **RSS 2022**
+
+*Haojie Huang, Dian Wang, Robin Walters, Robert Platt*
+
+**Abstract.** [Transporter Net](https://arxiv.org/pdf/2010.14406.pdf) is a recently proposed framework for pick and place that is able to learn good manipulation policies from a very few expert demonstrations. A key reason why Transporter Net is so sample efficient is that the model incorporates rotational equivariance into the pick module, i.e. the model immediately generalizes learned pick knowledge to objects presented in different orientations. This paper proposes a novel version of Transporter Net that is equivariant to both pick and place orientation. As a result, our model immediately generalizes place knowledge to different place orientations in addition to generalizing pick knowledge as before. Ultimately, our new model is more sample efficient and achieves better pick and place success rates than the baseline Transporter Net model.
+
+## Panda-gripper Simulation Environment for Pick and Place
+
+The experiment part of our paper is based on [Ravens](https://github.com/google-research/ravens), a simulation environment in PyBullet for 2D robotic manipulation with emphasis on pick and place. Since Ravens uses suction gripper that doesn't require pick angle inference, we select five tasks described below and provide a **Panda-Gripper version simulated Environment**. 
+It inherits the Gym-like API from Ravens, each with (i) a scripted oracle that provides expert demonstrations and (ii) reward functions that provide partial credit.
+
+<img src="image/gripper.png"><br>
+
+(a) **block-insertion**: pick up the L-shaped red block and place it into the L-shaped fixture.<br>
+(b) **place-red-in-green**: pick up the red blocks and place them into the green bowls amidst other objects.<br>
+(c) **align-box-corner**: pick up the randomly sized box and align one of its corners to the L-shaped marker on the tabletop.<br>
+(d) **stack-block-pyramid**: sequentially stack 6 blocks into a pyramid of 3-2-1 with rainbow colored ordering.<br>
+(e) **palletizing-boxes**: pick up homogeneous fixed-sized boxes and stack them in transposed layers on the pallet.<br>
+
+
+## Highlight
+
+It is worth noting that our proposed method, Equivariant Transporter Network can achieve more sample efficiency and faster converge speed. Taking the block-insertion task for example, it can hit ~100% success rate when trained with 1 expert demonstration for 200 SGD steps (less than 2 minutes). More results could be found in our paper.
+
+## Installation
+
+**Step 1.** Recommended: install [Miniconda](https://docs.conda.io/en/latest/miniconda.html) with Python 3.7.
+
+**Step 2.** Install Pytorch (Test on Pytorch 1.8.1 and Cuda 10.2)
+```commandline
+conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10.2 -c pytorch
+```
+**Step 3.** Install the required packages
+
+```shell
+pip install -r ./requirements.txt
+```
+(friendly note: It's a little awkward to install tensorflow that is not used for training. A tensorflow without cuda is ok since the model is trained with pytorch. Latter update will remove tensorflow and provied a pure-pytorch version that also enable batch training.)
+
+## Getting Started
+
+**Step 1.** Generate training and testing data.
+
+```shell
+python gripper_get_demo.py  --mode train --task block-insertion --n 10  --disp=True
+python gripper_get_demo.py  --mode test  --task block-insertion --n 100 --disp=True
+```
+
+
+**Step 2.** Train a model e.g., Equivariant Transporter. Parameters are saved to the `checkpoints_{model}` directory. 
+
+```shell
+python train.py --equ --lite --angle_lite --task=block-insertion --n_demos 1 --interval 200 --n_step 1000 --init
+```
+
+**Step 3.** Evaluate the model trained for 200 iterations with 1 demos. Results are saved to the `test_{model}` directory.
+
+```shell
+python gripper_test.py --equ --lite --angle_lite --task=block-insertion --n_demos 1 --n_steps 200 --disp=True
+```
+
+[//]: # (**Step 4.** Plot and print results.)
+
+## More description
+
+**Models:** Equivariant Transporter Network (`--equ`); Equivariant Transporter Network with a Tail Network(`--equ_tail`);  Pytorch-version Transporter Net(`--non`); Hybrid Models (`--femi` and `--semi`).
+Equivariant Transporter Network with a Tail Network performs well with a slightly long training time.
+
+**Observations:** RGB-D images (4X320X160)
+
+**Actions:** Picking: (u,v,theta); Placing: (u,v,theta)
+
+## Future Updates
+
+- Equivairant Transporter Network without Picking angle inference.
+- Pure Pytorch implementation
+- Batch training
+
---
+++ raven/gripper_assembling_kits.py
@@ -0,0 +1,272 @@
+# coding=utf-8
+# Copyright 2021 The Ravens Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Kitting Tasks."""
+
+import os
+import sys
+
+import numpy as np
+from gripper_task import Task
+import utils
+import random
+import matplotlib.pyplot as plt
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+
+
+class AssemblingKits(Task):
+  """Kitting Tasks base class."""
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    # self.ee = 'suction'
+    self.max_steps = 10
+    # self.metric = 'pose'
+    # self.primitive = 'pick_place'
+    self.train_set = np.arange(0, 14)
+    self.test_set = np.arange(14, 20)
+    self.homogeneous = False
+    self.task_name = 'assembling-kits'
+
+  def reset(self, env):
+    super().reset(env)
+
+    # Add kit.
+    kit_size = (0.28, 0.2, 0.005)
+    kit_urdf = 'kitting/kit.urdf'
+    kit_pose = self.get_random_pose(env, kit_size)
+    env.add_object(kit_urdf, kit_pose, 'fixed')
+
+    n_objects = 5
+    if self.mode == 'train':
+      obj_shapes = np.random.choice(self.train_set, n_objects)
+    else:
+      if self.homogeneous:
+        obj_shapes = [np.random.choice(self.test_set)] * n_objects
+      else:
+        obj_shapes = np.random.choice(self.test_set, n_objects)
+
+    colors = [
+        utils.COLORS['purple'], utils.COLORS['blue'], utils.COLORS['green'],
+        utils.COLORS['yellow'], utils.COLORS['red']
+    ]
+
+    symmetry = [
+        2 * np.pi, 2 * np.pi, 2 * np.pi / 3, np.pi / 2, np.pi / 2, 2 * np.pi,
+        np.pi, 2 * np.pi / 5, np.pi, np.pi / 2, 2 * np.pi / 5, 0, 2 * np.pi,
+        2 * np.pi, 2 * np.pi, 2 * np.pi, 0, 2 * np.pi / 6, 2 * np.pi, 2 * np.pi
+    ]
+
+    # Build kit.
+    targets = []
+    targ_pos = [[-0.09, 0.045, 0.0014], [0, 0.045, 0.0014],
+                [0.09, 0.045, 0.0014], [-0.045, -0.045, 0.0014],
+                [0.045, -0.045, 0.0014]]
+    template = 'kitting/object-template.urdf'
+    for i in range(n_objects):
+      shape = os.path.join(self.assets_root, 'kitting',
+                           f'{obj_shapes[i]:02d}.obj')
+      scale = [0.003, 0.003, 0.0001]  # .0005
+      pos = utils.apply(kit_pose, targ_pos[i])
+      theta = np.random.rand() * 2 * np.pi
+      rot = utils.eulerXYZ_to_quatXYZW((0, 0, theta))
+      replace = {'FNAME': (shape,), 'SCALE': scale, 'COLOR': (0.2, 0.2, 0.2)} # 0.61176471, 0.45882353, 0.37254902
+      urdf = self.fill_template(template, replace)
+      env.add_object(urdf, (pos, rot), 'fixed')
+      os.remove(urdf)
+      targets.append((pos, rot))
+
+    # Add objects.
+    objects = []
+    matches = []
+    # objects, syms, matcheses = [], [], []
+    for i in range(n_objects):
+      shape = obj_shapes[i]
+      size = (0.08, 0.08, 0.02)
+      pose = self.get_random_pose(env, size)
+      fname = f'{shape:02d}.obj'
+      fname = os.path.join(self.assets_root, 'kitting', fname)
+      scale = [0.003, 0.003, 0.001]  # .0005
+      replace = {'FNAME': (fname,), 'SCALE': scale, 'COLOR': colors[i]}
+      urdf = self.fill_template(template, replace)
+      block_id = env.add_object(urdf, pose)
+      os.remove(urdf)
+      objects.append((block_id, (symmetry[shape], None)))
+      # objects[block_id] = symmetry[shape]
+      match = np.zeros(len(targets))
+      match[np.argwhere(obj_shapes == shape).reshape(-1)] = 1
+      matches.append(match)
+      # print(targets)
+      # exit()
+      # matches.append(list(np.argwhere(obj_shapes == shape).reshape(-1)))
+    matches = np.int32(matches)
+    # print(matcheses)
+    # exit()
+
+    # Add goal.
+    # self.goals.append((objects, syms, targets, 'matches', 'pose', 1.))
+
+    # Goal: objects are placed in their respective kit locations.
+    # print(objects)
+    # print(matches)
+    # print(targets)
+    # exit()
+    self.goals.append((objects, matches, targets, False, True, 'pose', None, 1))
+    # goal = Goal(objects, syms, targets)
+    # metric = Metric('pose-matches', None, 1.)
+    # self.goals.append((goal, metric))
+
+    # # Goal: box is aligned with corner (1 of 4 possible poses).
+
+    # visualize the images 
+    # color, depth, segm = env.render_camera(self.oracle_cams[0])
+    # plt.imshow(depth, cmap='gray')
+    # plt.show()
+    # plt.imshow(color)
+    # plt.show()
+
+class AssemblingKitsTool(Task):
+  """Kitting Task - Plier."""
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.max_steps = 10
+    self.train_set = np.arange(0, 10)
+    self.test_set = np.arange(10, 15)
+    self.homogeneous = False
+    self.task_name = 'assembling-kits'
+
+  def reset(self, env):
+    super().reset(env)
+
+    tools = ['hammer', 'plier', 'screwdriver', 'wrench']
+    n_objects = [1, 1, 1, 1]
+
+    # Add kit
+    kit_size = (0.28, 0.26, 0.005)
+    kit_urdf = "plier/kit.urdf"
+    kit_pose = self.get_random_pose(env, kit_size)
+    env.add_object(kit_urdf, kit_pose, 'fixed')
+
+    obj_shapes = []
+    for i, tool in enumerate(tools):
+      if self.mode == 'train':
+        obj_shape = np.random.choice(self.train_set, n_objects[i])
+      else:
+        if self.homogeneous:
+          obj_shape = [np.random.choice(self.test_set)] * n_objects[i]
+        else:
+          obj_shape = np.random.choice(self.test_set, n_objects[i])
+      obj_shapes.append(obj_shape)
+
+    colors = [
+        utils.COLORS['purple'], utils.COLORS['blue'], utils.COLORS['green'],
+        utils.COLORS['yellow'], utils.COLORS['red']
+    ]
+    random.shuffle(colors)
+
+    symmetry = [
+        2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 
+        2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi, 2 * np.pi,
+        2 * np.pi
+    ]    
+
+    # # Build kit.
+    targets = []
+    targ_pos = [[-0.02, 0.03, 0.0014],
+                [0.02, -0.03, 0.0014],
+                [0.02, 0.09, 0.0014],
+                [-0.02, -0.09, 0.0014]]
+    template = 'plier/object-template.urdf'
+    for j, tool in enumerate(tools):
+      for i in range(n_objects[j]):
+        shape = os.path.join(self.assets_root, tool,
+                           f'{obj_shapes[j][i]:02d}.obj')
+        scale = [1, 1, 0.1]
+        pos = utils.apply(kit_pose, targ_pos[j]) # need modified
+
+        rot = utils.quatXYZW_to_eulerXYZ(kit_pose[1])
+        theta = rot[2] + (-1)** i * np.pi / 2
+        # theta = rot[2] + i % 2 * np.pi
+        rot = utils.eulerXYZ_to_quatXYZW((0, 0, theta))
+
+        replace = {'FNAME': (shape,), 'SCALE': scale, 'COLOR': (0.2, 0.2, 0.2)}
+        urdf = self.fill_template(template, replace)
+        env.add_object(urdf, (pos, rot), 'fixed')
+        os.remove(urdf)
+        targets.append((pos, rot))
+
+    # # visualize the depth images 
+    # color, depth, segm = env.render_camera(self.oracle_cams[0])
+    # plt.imshow(depth, cmap='gray')
+    # plt.show()
+    # plt.imshow(color)
+    # plt.show()
+
+    # Add objects.
+    # objects = []
+    # matches = []
+    # # objects, syms, matcheses = [], [], []
+    # for i in range(n_objects):
+    #   shape = obj_shapes[i]
+    #   size = (0.12, 0.15, 0.02)
+    #   pose = self.get_random_pose(env, size)
+    #   fname = f'{shape:02d}.obj'
+    #   fname = os.path.join(self.assets_root, 'plier', fname)
+    #   scale = [1, 1, 0.1]  # .0005
+    #   replace = {'FNAME': (fname,), 'SCALE': scale, 'COLOR': colors[i]}
+    #   urdf = self.fill_template(template, replace)
+    #   block_id = env.add_object(urdf, pose)
+    #   os.remove(urdf)
+    #   objects.append((block_id, (symmetry[shape], None)))
+    #   match = np.zeros(len(targets))
+    #   match[np.argwhere(obj_shapes == shape).reshape(-1)] = 1
+    #   matches.append(match)
+
+    # Add objects.
+    objects = []
+    matches = []
+    sizes =[[0.12, 0.15, 0.02],
+            [0.08, 0.15, 0.02],
+            [0.05, 0.15, 0.02],
+            [0.04, 0.15, 0.02]]
+    for j, tool in enumerate(tools):
+      for i in range(n_objects[j]):
+        shape = obj_shapes[j][i]
+        # size = (0.12, 0.15, 0.02)
+        size = sizes[j]
+        pose = self.get_random_pose(env, size)
+        fname = f'{shape:02d}.obj'
+        fname = os.path.join(self.assets_root, tool, fname)
+        scale = [1, 1, 1]  # .0005
+        replace = {'FNAME': (fname,), 'SCALE': scale, 'COLOR': colors[j]}
+        urdf = self.fill_template(template, replace)
+        block_id = env.add_object(urdf, pose)
+        # print('block_id', block_id, 'pose')
+        os.remove(urdf)
+        objects.append((block_id, (symmetry[shape], None)))
+        # match = np.zeros(len(targets))
+        # match[np.argwhere(obj_shapes[j] == shape).reshape(-1)] = 1
+        # matches.append(match)
+
+    matches = [[1,0,0,0],
+               [0,1,0,0],
+               [0,0,1,0],
+               [0,0,0,1]]
+    
+    matches = np.int32(matches)
+    self.goals.append((objects, matches, targets, False, True, 'pose', None, 1))  
