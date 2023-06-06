d9ff77527086b4293a1fd9aee0c906463e219da1
Jadon <jiadong001@e.ntu.edu.sg>
2023-06-06 10:46:11 +0800

    Add fully quotient version of the pick angle network

--- networks/so2_pick_angle_model.py
+++ networks/so2_pick_angle_model.py
@@ -19,31 +19,29 @@ class SO2ResBlock(torch.nn.Module):
             self.r2_act = gspaces.flipRot2dOnR2(N=N)
         else:
             self.r2_act = gspaces.rot2dOnR2(N=N)
-
-        # if quotient:
-        #     if flip:
-        #         rep = r2_act.quotient_repr((None, 2))
-        #     else:
-        #         rep = r2_act.quotient_repr(2)
-        # else:
-        #     rep = r2_act.regular_repr
         
-        self.rho = self.r2_act.fibergroup.spectral_regular_representation(*irreps, name=None)
+        if quotient:
+            self.rho = self.r2_act.fibergroup.spectral_quotient_representation(2, *irreps, name=None)
+            activation = nn.QuotientFourierELU(self.r2_act, 2, output_channels, irreps, N=num_of_samples, inplace=True)
+            last_activation = nn.QuotientFourierELU(self.r2_act, 2, output_channels, irreps, N=num_of_samples, inplace=True)
+        else:
+            self.rho = self.r2_act.fibergroup.spectral_regular_representation(*irreps, name=None)
+            activation = nn.FourierELU(self.r2_act, output_channels, irreps=irreps, N=num_of_samples, inplace=True)
+            last_activation = nn.FourierELU(self.r2_act, output_channels, irreps=irreps, N=num_of_samples, inplace=True)
+
         feat_type_in = nn.FieldType(self.r2_act, [self.rho] * input_channels)
         feat_type_out = nn.FieldType(self.r2_act, [self.rho] * output_channels)
-
+        
         self.layer1 = nn.SequentialModule(
             nn.R2Conv(feat_type_in, feat_type_out, kernel_size=kernel_size, padding=(kernel_size-1) // 2, bias=False, initialize=initialize),
-            nn.FourierELU(self.r2_act, output_channels, irreps=irreps, N=num_of_samples, inplace=True),
+            activation,
         )
 
         self.layer2 = nn.SequentialModule(
             nn.R2Conv(feat_type_out, feat_type_out, kernel_size=kernel_size, padding=(kernel_size-1) // 2, bias=False, initialize=initialize),
         )
 
-        self.last_act =last_act
-        if self.last_act:
-            self.last_activation = nn.FourierELU(self.r2_act, output_channels, irreps=irreps, N=num_of_samples, inplace=True)
+        self.last_activation = last_activation if last_act else None
 
         self.upscale = None
         if input_channels != output_channels:
@@ -61,7 +59,7 @@ class SO2ResBlock(torch.nn.Module):
         else:
             out += residual
         
-        if self.last_act:
+        if self.last_activation:
             out = self.last_activation(out)
 
         return out
@@ -74,7 +72,7 @@ class SO2ResNet(torch.nn.Module):
                  kernel_size=7,
                  N=36, 
                  flip=False,
-                 quotient=True,
+                 quotient=False,
                  initialize=False,
                  init=False,
                  **irrep_kwargs):
@@ -82,7 +80,7 @@ class SO2ResNet(torch.nn.Module):
         max_irrep = irrep_kwargs['irrep']
         num_of_samples = irrep_kwargs['sample']
 
-        N_out = N # descrite group
+        N_out = N # discrete group for output
         N = -1 # infinite group
 
         if flip:
@@ -92,8 +90,11 @@ class SO2ResNet(torch.nn.Module):
         
         irreps = [(f,) for f in range(max_irrep+1)]
         self.irreps = irreps
-        self.rho = self.r2_act.fibergroup.spectral_regular_representation(*(self.irreps), name=None)
-        self.rho_q= self.r2_act.fibergroup.spectral_quotient_representation(2, *(self.irreps), name=None)
+
+        if quotient:
+            self.rho = self.r2_act.fibergroup.spectral_quotient_representation(2, *(self.irreps), name=None)
+        else:
+            self.rho = self.r2_act.fibergroup.spectral_regular_representation(*(self.irreps), name=None)
 
         self.l1_c = n_middle_channels[0]
         self.l2_c = n_middle_channels[1]
@@ -101,11 +102,13 @@ class SO2ResNet(torch.nn.Module):
         self.l4_c = n_middle_channels[3]
         self.l5_c = n_middle_channels[4]
 
+        activation_e0 = nn.QuotientFourierELU(self.r2_act, 2, self.l1_c, irreps=self.irreps, N=num_of_samples, inplace=True) if quotient \
+              else nn.FourierELU(self.r2_act, self.l1_c, irreps=self.irreps, N=num_of_samples, inplace=True)
         self.conv_down_1 = torch.nn.Sequential(OrderedDict([
             ('enc-e2conv-0', nn.R2Conv(nn.FieldType(self.r2_act, [self.r2_act.trivial_repr]*n_input_channel),
                                        nn.FieldType(self.r2_act, [self.rho]*self.l1_c),
                                        kernel_size=kernel_size,padding=3,bias=False,initialize=initialize)),
-            ('enc-e2elu-0', nn.FourierELU(self.r2_act, self.l1_c, irreps=self.irreps, N=num_of_samples, inplace=True)),
+            ('enc-e2elu-0', activation_e0),
             ('enc-e2res-1', SO2ResBlock(self.l1_c,self.l1_c,kernel_size=kernel_size,N=N,irreps=self.irreps,num_of_samples=num_of_samples,flip=flip,quotient=quotient,initialize=initialize)),
         ]))
 
@@ -162,16 +165,20 @@ class SO2ResNet(torch.nn.Module):
                                         # kernel_size=2,stride=1,padding=0)
 
         # 16X16X16IR -> 4X4X1IQ
+        activation_f0 = nn.QuotientFourierELU(self.r2_act, 2, self.l4_c, irreps=self.irreps, N=num_of_samples, inplace=True) if quotient \
+              else nn.FourierELU(self.r2_act, self.l4_c, irreps=self.irreps, N=num_of_samples, inplace=True)
+        self.rho_last= self.r2_act.fibergroup.spectral_quotient_representation(2, *(self.irreps), name=None)
         self.final_0 = torch.nn.Sequential(OrderedDict([
             ('enc-final-0', nn.R2Conv(nn.FieldType(self.r2_act, [self.rho]*self.l3_c),
                                       nn.FieldType(self.r2_act, [self.rho]*self.l4_c),
                                       kernel_size=kernel_size,padding=0,bias=False,initialize=initialize)),
-            ('enc-f_elu-0', nn.FourierELU(self.r2_act, self.l4_c, irreps=self.irreps, N=num_of_samples, inplace=True)),
+            ('enc-f_elu-0', activation_f0),
             ('enc-final-0-1', nn.R2Conv(nn.FieldType(self.r2_act, [self.rho]*self.l4_c),
-                                        nn.FieldType(self.r2_act, [self.rho_q]*n_output_channel),
+                                        nn.FieldType(self.r2_act, [self.rho_last]*n_output_channel),
                                         kernel_size=kernel_size,padding=0,bias=False,initialize=initialize)),         
         ]))
 
+        # 4X4X1IQ -> 1X1X1Q
         self.r2_act_out = gspaces.rot2dOnR2(N=N_out)
         self.final_1 = torch.nn.Sequential(OrderedDict([
                 ('discrete_map', QIFTPointwise(self.r2_act,self.r2_act_out,2,n_output_channel,irreps=self.irreps,N=self.r2_act_out.regular_repr.size)),
@@ -179,6 +186,7 @@ class SO2ResNet(torch.nn.Module):
         self.pool = nn.PointwiseAvgPool(nn.FieldType(self.r2_act_out, [self.r2_act_out.quotient_repr(2)]*n_output_channel),
                                         kernel_size=4,stride=1,padding=0)
         
+        # initilize weights 
         for name, module in self.named_modules():
             if isinstance(module, nn.R2Conv):
                 if init:
