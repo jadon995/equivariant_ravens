730c6e78844c7d29aadda54af05509827ee240cd
Jadon <jiadong001@e.ntu.edu.sg>
2023-06-09 13:38:58 +0800

    Debug the permutation of the label

--- networks/label/smooth_label_3d.py
+++ networks/label/smooth_label_3d.py
@@ -11,14 +11,15 @@ def get_gaussian_3d_label(map_size, center, radius,
                           dtype=torch.float32, device='cpu'):
     heatmap_2d = gen_gaussian_label(map_size[1:], center[1:], radius=radius, sigma=radius,
                                     normalized=True, dtype=dtype, device=device)
-    angle_label = get_angle_smooth_label(center[0], angle_range=360, radius=radius, normalized=True)
+    angle_label = get_angle_smooth_label(center[0], angle_range=map_size[0], radius=radius, normalized=True)
     angle_label = torch.as_tensor(angle_label,dtype=torch.float32,device=device)
     heatmap_2d = heatmap_2d.unsqueeze(dim=-1)
     heatmap_3d = heatmap_2d * angle_label
-    heatmap_3d = heatmap_3d.reshape(*map_size)
+    heatmap_3d = heatmap_3d.permute(2,0,1)
 
     return heatmap_3d
 
 if __name__ == '__main__':
-    heatmap_3d = get_gaussian_3d_label((180, 10, 10), (90, 4, 4), radius=1, device='cuda')
+    heatmap_3d = get_gaussian_3d_label((360, 10, 10), (90, 4, 4), radius=1, device='cuda')
     print(heatmap_3d.shape)
+    print(heatmap_3d[90,4,4])
